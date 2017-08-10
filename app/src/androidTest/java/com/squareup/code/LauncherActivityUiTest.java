178e46b122eaf5cfe01a8262ce9f66cf9b293b48
liangzhenxiong <liangzhenxiong@imay.com>
2017-08-10 14:08:12 +0800

    完成了启动页UI测试

---
+++ app/src/androidTest/java/com/squareup/code/LauncherActivityUiTest.java
@@ -0,0 +1,66 @@
+package com.squareup.code;
+
+/**
+ * Created by Administrator on 2017/08/10 0010.
+ */
+
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.InstrumentationRegistry.getInstrumentation;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static org.junit.Assert.assertEquals;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class LauncherActivityUiTest {
+
+    private Intent mLoginIntent;
+
+
+
+    public LauncherActivityUiTest() {
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        mLoginIntent = new Intent(getInstrumentation().getTargetContext(), LauncherActivity.class);
+    }
+
+    @Rule
+    public ActivityTestRule<LauncherActivity> mActivityRule = new ActivityTestRule<>(
+            LauncherActivity.class);
+
+//    @Before
+//    public void initValidString() {
+//        // Specify a valid string.
+//        mStringToBetyped = "Espresso";
+//    }
+
+    @Test
+    public void changeText_sameActivity() {
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("com.squareup.code", appContext.getPackageName());
+        // Type text and then press the button.
+//        onView(withId(R.id.editTextUserInput))
+//                .perform(typeText(mStringToBetyped), closeSoftKeyboard());
+        onView(withId(R.id.test_tv_radio)).perform(click());
+//
+//        // Check that the text was changed.
+//        onView(withId(R.id.textToBeChanged))
+//                .check(matches(withText(mStringToBetyped)));
+    }
+}
---
+++ app/src/test/java/com/squareup/code/HomeActivityTest.java
@@ -0,0 +1,56 @@
+package com.squareup.code;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.android.controller.ActivityController;
+
+import java.lang.reflect.Method;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Administrator on 2017/08/10 0010.
+ */
+@RunWith(RobolectricTestRunner.class)
+public class HomeActivityTest {
+    @Test
+    public void onCreate() throws Exception {
+
+    }
+
+    @Test
+    public void isAllTranslucentStatus() throws Exception {
+        ActivityController<HomeActivity> activityController = Robolectric.buildActivity(HomeActivity.class).create().start();
+        HomeActivity homeActivity = activityController.get();
+        Method method = (MethodUtil.getMethod(homeActivity, "isAllTranslucentStatus", new Class[0]));
+        assertTrue(((boolean) method.invoke(homeActivity)));
+    }
+
+    @Test
+    public void onEventMain() throws Exception {
+
+    }
+
+    @Test
+    public void onBackPressed() throws Exception {
+
+    }
+
+    @Test
+    public void onEventThread() throws Exception {
+
+    }
+
+    @Test
+    public void onClick() throws Exception {
+
+    }
+
+    @Test
+    public void onActivityResult() throws Exception {
+
+    }
+
+}
